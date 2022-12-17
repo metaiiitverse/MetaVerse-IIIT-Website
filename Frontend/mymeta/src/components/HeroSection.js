b3a20a7786349dcf9f67bf9fb0684c48e385be48
sneha_mandal <snehacreative2016@gmail.com>
2022-12-17 09:36:59 +0530

    changes

--- Frontend/mymeta/src/components/HeroSection.js
+++ Frontend/mymeta/src/components/HeroSection.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function HeroSection() {
+
+  const[initialInput,setInitialInput]=useState({
+    name:"",
+    branch:"",
+    email:"",
+    password:"",
+  })
+
+  const handleForm=(e)=>{
+    
+    const{name,value}=e.target
+    setInitialInput({
+      ...initialInput,
+      [name]:value,
+    })
+
+    console.log(initialInput)
+  }
 
 
 
   
   return (
     <div>
-        <section className="text-gray-400 bg-gray-900 body-font">
+        <section className="text-gray-400 bg-gray-900 body-font heroBox">
   <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
     <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
       <h1 className="title-font font-medium text-3xl text-white">Welcome to meta</h1>
@@ -17,19 +35,19 @@ function HeroSection() {
       <h2 className="text-white text-lg font-medium title-font mb-5">Sign Up</h2>
       <div className="relative mb-4">
         <label for="full-name" className="leading-7 text-sm text-gray-400">Full Name</label>
-        <input type="text" id="full-name" name="full-name" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="text" id="full-name" name="full-name" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleForm}/>
       </div>
       <div className="relative mb-4">
         <label for="full-name" className="leading-7 text-sm text-gray-400">Branch</label>
-        <input type="text" id="branch" name="branch" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="text" id="branch" name="branch" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleForm}/>
       </div>
       <div className="relative mb-4">
         <label for="email" className="leading-7 text-sm text-gray-400">Email</label>
-        <input type="email" id="email" name="email" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="email" id="email" name="email" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"onChange={handleForm}/>
       </div>
       <div className="relative mb-4">
         <label for="email" className="leading-7 text-sm text-gray-400">Password</label>
-        <input type="password" id="password" name="password" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+        <input type="password" id="password" name="password" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"onChange={handleForm}/>
       </div>
       <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Register</button>
       <p className="text-xs mt-3">the furture of internet awaits </p>
